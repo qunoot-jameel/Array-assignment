cebacabb90771d5bce573ec3965992f021fc6f70
qunoot-jameel <qunootjameel@gmail.com>
2024-05-28 22:41:45 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,52 @@
+         //Question 1
+
+let fruits:string[]=["apple","banana","mango","orange"];
+console.log(fruits);
+          
+        //Question 2
+let numbers:number[]=[10,20,30,40];
+console.log(numbers);
+
+       //Question 3
+let selectFruits=fruits[2];
+console.log(selectFruits);
+
+        //Question 4
+numbers[1]=25;
+console.log(numbers);
+
+        //Question 5
+fruits.push("grapes");
+console.log(fruits);
+
+         //Question 6
+let lastFruit=fruits.pop();
+console.log(fruits);
+
+         //Question 7
+let firstFruit=fruits.shift();
+console.log(fruits);
+
+        //Question 8
+fruits.unshift("kiwi");
+console.log(fruits);
+
+        //Question 9
+fruits.splice(1,2);
+console.log(fruits);
+
+         //Question 10
+fruits.splice(2,0,"pineapple","pear");
+console.log(fruits);
+
+          //Question 11
+let citrusFruits:string[]=fruits.slice(0,2);
+console.log(fruits);
+
+          //Question 12
+let lastTwoFruits:string[]=fruits.slice(-2);
+console.log(fruits);
+
+
+
+
